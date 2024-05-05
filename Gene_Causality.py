b17c891d44f79b73b779afb3ffa70e4d027c281e
kjl3467 <kieranlynch5@yahoo.com>
2024-05-05 19:51:32 -0400

    cleaned

--- Gene_Causality.py
+++ Gene_Causality.py
@@ -11,9 +11,12 @@ from sklearn.metrics import accuracy_score, mean_squared_error, confusion_matrix
 import scipy.stats as stats
 from sklearn.ensemble import RandomForestRegressor
 from sklearn.model_selection import GridSearchCV
+from scipy.sparse import csr_matrix,vstack,save_npz,load_npz
 from xgboost import XGBRegressor 
 import pickle
-
+"""
+ Need to add an orientation option  
+"""
 class GeneCasuality_Search:
     def __init__(self):
         self.tf_masterlist = []
@@ -30,6 +33,7 @@ class GeneCasuality_Search:
         self.train = None
         self.test = None
         self.gene_performance = None
+        self.gene_column = None
 
     def __str__(self):
         if self.tf_data is not None and self.cam_data is not None:  # Changed condition to check if data is not None
@@ -51,20 +55,38 @@ class GeneCasuality_Search:
         self.working_data = self.__alldata.copy() if self.__alldata is not None else None
         self.working_metadata = self.__allmetadata.copy() if self.__allmetadata is not None else None
 
-    def read_genomic_data(self, early_path, main_path):
-        early = mmread(early_path).transpose().astype(np.uint8).tocsr()
-        main = mmread(main_path).transpose().astype(np.uint8).tocsr()
-        self.__alldata = vstack([main, early])
+    def read_genomic_data(self, rows, *paths):
+        """
+        Must be of type .npz matrices. 
+
+        Added a row parameter which specifies if genes or samples are on the rows
+        """
+        expression = None
+        for path in paths:
+            expression = load_npz(path) if expression is None else vstack([expression, load_npz(path)])
+        
+        if rows == 'genes':
+            self.__alldata = expression.T
+        else:
+            self.__alldata = expression
         self.working_data = self.__alldata.copy()
 
-    def read_genes(self, feature_path):
-        all_info = pd.read_csv(feature_path, names=['fbname', 'gene_name', 'expression'], delimiter='\t')
+    def read_genes(self, feature_path,gene_column='Genes',delimiter=','):
+        """
+        Needs to be either a csv or tsv. OR pkl file
+        """
+        if feature_path.split('.')[-1] == 'pkl':
+            with open(feature_path,'rb') as f:
+                all_info = pd.DataFrame({gene_column:pickle.load(f)})
+        else:
+            all_info = pd.read_csv(feature_path, delimiter=delimiter)
+        self.gene_column = gene_column
         self.__genenames = all_info
         self.working_genes = all_info.copy()
 
     def subset_tf_cam(self):
-        tf_indices = list(self.__genenames[self.__genenames['gene_name'].str.lower().isin(self.tf_masterlist)].index)
-        cam_indices = list(self.__genenames[self.__genenames['gene_name'].str.lower().isin(self.cam_masterlist)].index)
+        tf_indices = list(self.__genenames[self.__genenames[self.gene_column].str.lower().isin(self.tf_masterlist)].index)
+        cam_indices = list(self.__genenames[self.__genenames[self.gene_column].str.lower().isin(self.cam_masterlist)].index)
         master_indices = tf_indices + cam_indices
         self.tf_data = self.working_data[:, tf_indices]
         self.cam_data = self.working_data[:, cam_indices]
@@ -74,7 +96,6 @@ class GeneCasuality_Search:
     def read_metadata(self, early_metadata_path, main_metadata_path, feature_path):
         metadata_main = pd.read_csv(main_metadata_path, sep='\t')
         metadata_early = pd.read_csv(early_metadata_path, sep='\t')
-        genes = pd.read_csv(feature_path, sep='\t', names=['gene_id', 'gene_name', 'analysis'])
         metadata_main.set_index('barcode', inplace=True)
         metadata_early.set_index('barcode', inplace=True)
         metadata_combined = pd.concat([metadata_main, metadata_early]).reset_index()
@@ -82,10 +103,14 @@ class GeneCasuality_Search:
         self.working_metadata = metadata_combined.copy()
 
     def read_master_genes(self, tf_path, cam_path):
-        tf_list = pd.read_csv(tf_path).iloc[:, 0].str.lower().tolist()
-        cam_list = pd.read_csv(cam_path).iloc[:, 0].str.lower().tolist()
-        self.cam_masterlist += cam_list
-        self.tf_masterlist += tf_list
+
+        """
+        This will read in the list of genes whihc belong to categories Transcription Factor and Cellular Adhesion Molecule. 
+
+        TF path goes FIRST and CAM Path goes SECOND
+        """
+        self.cam_masterlist = pd.read_csv(cam_path).iloc[:, 0].str.lower().tolist()
+        self.tf_masterlist = pd.read_csv(tf_path).iloc[:, 0].str.lower().tolist()
         common_genes = set(self.cam_masterlist) & set(self.tf_masterlist)
         if common_genes:
             print('Found gene_names which belonged to both CAM & TF:')
@@ -114,13 +139,15 @@ class GeneCasuality_Search:
         preds = self.trained_model.predict(self.test[0])
         overall_performance = np.mean(np.abs(self.test[1].toarray() - np.round(preds)))
         gene_by_gene_performance = np.mean(np.abs(self.test[1].toarray() - np.round(preds)), axis=0)
-        return overall_performance, pd.DataFrame(gene_by_gene_performance, columns=['Performance'])
+        print(overall_performance ," Mean Average Error")
+        return pd.DataFrame(gene_by_gene_performance, columns=['Performance'])
 
     def reduce_feature(self):
         rf = RandomForestRegressor(n_jobs=-1, max_depth=5)
         feat_selector = BorutaPy(rf, n_estimators='auto', verbose=2, random_state=1)
         X_filtered = feat_selector.fit_transform(self.tf_data.toarray(), self.cam_data.toarray())
         return X_filtered
+    
     def grid_search(self,param_grid):
         X_train, X_test, y_train, y_test = train_test_split(self.tf_data, self.cam_data, test_size=5e-2)
         xgb_model = XGBRegressor(tree_method='gpu_hist', objective='reg:squarederror', verbosity=2)
